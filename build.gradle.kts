db3e4a9ba6eaad6a9601b08b860f69f7d85b8674
PHZDYZ2395 <jmgr2395@leeds.ac.uk>
2026-05-07 19:08:43 +0800

    build: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.21"
+    kotlin("plugin.jpa") version "1.9.21"
 }
 
 group = "HealthyEat"
@@ -10,6 +14,28 @@ repositories {
 }
 
 dependencies {
+    // Spring Boot Core
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // Kotlin Support
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
+
+    // Database
+    runtimeOnly("com.h2database:h2")
+
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
     testImplementation(kotlin("test"))
 }
 
@@ -17,5 +43,8 @@ tasks.test {
     useJUnitPlatform()
 }
 kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
     jvmToolchain(17)
 }
